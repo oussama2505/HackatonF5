0d257490cafe3dc24cfacde5c3feca28bd27c32e
EliasXVIII <elias.renault4@gmail.com>
2024-05-30 12:25:12 +0200

    function Select *

--- Backend/main.py
+++ Backend/main.py
@@ -21,11 +21,24 @@ print("-----------------------------------------------------------")
 conexion1=mysql.connector.connect(host="localhost", 
                                   user="root", 
                                   passwd="", 
-                                  database="pruebaElias")
+                                  database="alumnos")
 cursor1=conexion1.cursor()
 cursor1.execute("show tables")
 for tabla in cursor1:
     print(tabla)
+""" conexion1.close() """
+import mysql.connector
+
+
+## Con esto hacemos la consulta! 
+conexion1=mysql.connector.connect(host="localhost", 
+                                  user="root", 
+                                  passwd="", 
+                                  database="alumnos")
+cursor1=conexion1.cursor()
+cursor1.execute("select * from alumno_tabla")
+for fila in cursor1:
+    print(fila)
 conexion1.close()    
 
 if __name__ == "__main__":
