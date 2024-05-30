e792035e05fd1087b3bd81b35b796564c1ff436d
EliasXVIII <elias.renault4@gmail.com>
2024-05-30 10:31:29 +0200

    connecting to the DATABASE

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import mysql.connector
 
 
 app = Flask(__name__)
@@ -6,6 +7,26 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+conexion1=mysql.connector.connect(host="localhost", user="root", passwd="")
+cursor1=conexion1.cursor()
+cursor1.execute("show databases")
+for base in cursor1:
+    print(base)
+conexion1.close()   
+
+print("-----------------------------------------------------------")
+
+conexion1=mysql.connector.connect(host="localhost", 
+                                  user="root", 
+                                  passwd="", 
+                                  database="pruebaElias")
+cursor1=conexion1.cursor()
+cursor1.execute("show tables")
+for tabla in cursor1:
+    print(tabla)
+conexion1.close()    
 
 if __name__ == "__main__":
     app.run(debug=True, port="4000", host="0.0.0.0")
