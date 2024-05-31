368775eebf52a03f9002831ac67ead4509243722
oussama2505 <oussama.brahmi0608@gmail.com>
2024-05-31 14:17:35 +0200

    add Dashboard Component

--- Frontend/hackathon-admin/src/Views/Dashboard.vue
+++ Frontend/hackathon-admin/src/Views/Dashboard.vue
@@ -1,22 +1,19 @@
 <template>
+     <Header />
     <div>
       <h1>Dashboard del Administrador</h1>
       <upload-form></upload-form>
       <teams-list></teams-list>
     </div>
+    <Footer />
   </template>
   
 
-  <script>
+  <script setup>
   import UploadForm from '../components/UploadForm.vue';
   import TeamsList from '../components/TeamsList.vue';
+  import Header from '../components/Header.vue';
+  import Footer from '../components/Footer.vue';
   
-  export default {
-    name: 'Dashboard',
-    components: {
-      UploadForm,
-      TeamsList
-    }
-  };
   </script>
   
