410d1e99f01cd5bc8dceb53f49659a5643f9f99b
EliasXVIII <elias.renault4@gmail.com>
2024-05-31 12:08:07 +0200

    Created the API with its respective results. connected the database with the back ok

--- Backend/connect_mysql.py
+++ Backend/connect_mysql.py
@@ -1,6 +1,5 @@
 import mysql.connector
 
-
 conexion1=mysql.connector.connect(host="localhost", user="root", passwd="")
 cursor1=conexion1.cursor()
 cursor1.execute("show databases")
@@ -18,11 +17,7 @@ cursor1=conexion1.cursor()
 cursor1.execute("show tables")
 for tabla in cursor1:
     print(tabla)
-""" conexion1.close() """
 
-
-
-## Con esto hacemos la consulta! 
 conexion1=mysql.connector.connect(host="localhost", 
                                   user="root", 
                                   passwd="", 
--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify
 from connect_mysql import *
-
+import mysql.connector
 
 app = Flask(__name__)
 
@@ -8,7 +8,18 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-
+@app.route('/api/personas', methods=['GET'])
+def get_personas():
+    db = mysql.connector.connect(host="localhost", 
+                                  user="root", 
+                                  passwd="", 
+                                  database="alumnos")
+    cursor1 = db.cursor(dictionary=True)
+    cursor1.execute("SELECT * FROM alumno_tabla")
+    personas = cursor1.fetchall()
+    cursor1.close()
+    db.close()
+    return jsonify(personas)
 
 if __name__ == "__main__":
     app.run(debug=True, port="4000", host="0.0.0.0")
