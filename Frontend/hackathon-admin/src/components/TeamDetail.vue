589c744fb2ef9fa70bb1fbeb22fc1af446948e08
oussama2505 <oussama.brahmi0608@gmail.com>
2024-05-31 18:22:48 +0200

    Define Routes

--- Frontend/hackathon-admin/src/components/TeamDetail.vue
+++ Frontend/hackathon-admin/src/components/TeamDetail.vue
@@ -1,28 +1,15 @@
 <template>
     <div>
-      <h3>{{ team.name }}</h3>
-      <p>{{ team.description }}</p>
+      <h3>team.name </h3>
+      <p> team.description </p>
       <ul>
-        <li v-for="participant in team.participants" :key="participant.id">
-          {{ participant.name }} ({{ participant.stack }})
+        <li> teams lists
         </li>
       </ul>
     </div>
   </template>
   
   <script>
-  import { defineProps } from 'vue';
-  
-  export default {
-    name: 'TeamDetail',
-    props: {
-      team: Object
-    },
-    setup(props) {
-      return {
-        team: props.team
-      };
-    }
-  };
+ 
   </script>
   
