b1f78e59f9e0ede10763e599d177284b63cd5abb
EliasJavierRiquelme <elias.renault4@gmail.com>
2024-06-02 19:22:13 +0200

    fail backend csv.

--- Backend/main.py
+++ Backend/main.py
@@ -1,8 +1,36 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from connect_mysql import *
 import mysql.connector
+import csv
 
 app = Flask(__name__)
+
+@app.route('/api/upload', methods=['POST'])
+def upload_file():
+    file = request.files['file']
+    if file:
+        # Leer el archivo CSV
+        csv_data = csv.reader(file.stream, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+        # Conectar a la base de datos MySQL
+        db = mysql.connector.connect(host="localhost", user="root", passwd="", database="alumnos")
+        cursor = db.cursor()
+
+        # Iterar sobre las filas del archivo CSV e insertar en la base de datos
+        for row in csv_data:
+            nombre, apellido, front, back, email, bootcamp = row
+            cursor.execute("INSERT INTO alumno_tabla (nombre, apellido, front, back, email, bootcamp) VALUES (%s, %s, %s, %s, %s, %s)", (nombre, apellido, front, back, email, bootcamp))
+
+        # Confirmar los cambios y cerrar la conexión
+        db.commit()
+        cursor.close()
+        db.close()
+
+        return jsonify({'message': 'Archivo CSV cargado correctamente'}), 200
+    else:
+        return jsonify({'error': 'No se proporcionó ningún archivo CSV'}), 400
+
+
 
 @app.route('/api/personas', methods=['GET'])
 def get_personas():
