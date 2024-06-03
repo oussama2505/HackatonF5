7e32c68077622a0af12fbd78b03e9bc145b0df36
EliasXVIII <elias.renault4@gmail.com>
2024-06-03 11:56:18 +0200

    add Swagger Docs

--- Backend/main.py
+++ Backend/main.py
@@ -1,11 +1,24 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, redirect
 from connect_mysql import *
 import mysql.connector
 from flask_cors import CORS
-
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 CORS(app)
+
+SWAGGER_URL="/swagger"
+API_URL="/static/swagger.json"
+
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': 'Access API'
+    }
+)
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
+
 
 
 def connect_db():
