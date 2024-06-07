02f0bb8bc86d346c57631f7d0dac9de0e8214c49
usertina <agustinacalleja@gmail.com>
2024-06-07 11:57:21 +0200

    resolve conflicts

--- Frontend/hackathon-admin/src/components/Login.vue
+++ Frontend/hackathon-admin/src/components/Login.vue
@@ -11,6 +11,7 @@
           <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
           <div class="mt-2">
             <input v-model="email" id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+            <p v-if="!email" class="text-sm text-red-500">Please enter your email address.</p>
           </div>
         </div>
 
@@ -20,6 +21,7 @@
           </div>
           <div class="mt-2">
             <input v-model="password" id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+            <p v-if="!password" class="text-sm text-red-500">Please enter your password.</p>
           </div>
         </div>
 
@@ -27,11 +29,10 @@
           <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
         </div>
       </form>
-
-
     </div>
   </div>
 </template>
+
 <script>
 import axios from 'axios';
 
@@ -45,6 +46,9 @@ export default {
   },
   methods: {
     async login() {
+      if (!this.email || !this.password) {
+        return; // Impide el envío del formulario si los campos están vacíos
+      }
       try {
         const response = await axios.post('http://localhost:4000/api/login', {
           email: this.email,
@@ -55,8 +59,7 @@ export default {
       } catch (error) {
         alert('Invalid credentials');
       }
-    },
-
+    }
   }
 };
 </script>
