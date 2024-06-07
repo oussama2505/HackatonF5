b80f90024ad277600e4d128cfc7cd5bbb7b1737c
usertina <agustinacalleja@gmail.com>
2024-06-07 12:03:14 +0200

    resolve port tina

--- Backend/main.py
+++ Backend/main.py
@@ -34,7 +34,6 @@ app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 def connect_db():
     return mysql.connector.connect(
         host="localhost",
-        port= "3305",
         user="root",
         password="",
         database="alumnos"
@@ -106,7 +105,7 @@ def upload_file():
 
 @app.route('/api/grupos', methods=['GET'])
 def get_grupos():
-    db = mysql.connector.connect(host="localhost", port= "3305", user="root", passwd="", database="alumnos")
+    db = mysql.connector.connect(host="localhost", user="root", passwd="", database="alumnos")
     cursor1 = db.cursor(dictionary=True)
     cursor1.execute("SELECT * FROM alumno_tabla")
     personas = cursor1.fetchall()
